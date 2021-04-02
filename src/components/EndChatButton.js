1082f410a382e1c145993a77a32e744ee8844262
Scott Rollan <b.scott.rollan@gmail.com>
2021-04-01 23:12:34 -0400

    style and performance upgrades

--- src/components/EndChatButton.js
+++ src/components/EndChatButton.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { Button, Modal } from 'react-bootstrap';
+import { helpChatsCollection } from '../firestore/index';
 
 export default function EndChatButton({ str, chatObj }) {
-  const [show, setShow] = useState(true);
+  const [show, setShow] = useState(false);
 
   const sendEmail = () => {
     const user = chatObj.userName;
@@ -12,7 +13,7 @@ export default function EndChatButton({ str, chatObj }) {
     const chatStartTime = convArray[0].timestamp;
     const convLength = convArray.length;
     const chatEndTime = convArray[convLength - 1].timestamp;
-    const header = `This chat was initiated with Delightful Dog at ${chatStartTime.toDate()} by ${user} (with a provided email of ${userEmail}) and ended at ${chatEndTime.toDate()}: `;
+    const header = `Chat initiated: ${chatStartTime.toDate()} // by: ${user} //with a provided email of: ${userEmail} // Chat ended: ${chatEndTime.toDate()}: `;
     convArray.forEach((c) => {
       if (c.fromUser) {
         conversation = [...conversation, `<p>${user} said: ${c.quote}</p><br>`];
@@ -23,9 +24,26 @@ export default function EndChatButton({ str, chatObj }) {
     console.log(`${header}${conversation}`);
   };
 
+  const endConversation = () => {
+    setShow(true);
+  };
+
+  const deleteChat = () => {
+    const id = chatObj.id;
+    helpChatsCollection
+      .doc(id)
+      .delete()
+      .then(() => {
+        alert('Chat permanently deleted');
+      });
+  };
+
   return (
     <>
       <Modal show={show} onHide={() => setShow(false)}>
+        <Modal.Header
+          closeButton
+        >{`Chat with ${chatObj.userName}`}</Modal.Header>
         <Modal.Body>
           <div style={styles.buttonDiv}>
             <Button
@@ -33,17 +51,30 @@ export default function EndChatButton({ str, chatObj }) {
               style={styles.button}
               onClick={() => sendEmail()}
             >
-              Email Transcript to Delightful Dog
+              Email Chat Transcript to Delightful Dog
             </Button>
-            <Button variant="outline-danger" style={styles.button}>
-              Close Conversation Permanently
+            <Button
+              variant="outline-danger"
+              style={styles.button}
+              onClick={() => deleteChat()}
+            >
+              Delete Chat Permanently
+            </Button>
+          </div>
+          <div style={styles.buttonDiv}>
+            <Button
+              variant="outline-primary"
+              style={styles.button}
+              onClick={() => setShow(false)}
+            >
+              Close This Window
             </Button>
           </div>
         </Modal.Body>
       </Modal>
       <Button
         variant="outline-info"
-        onClick={() => console.log(chatObj)}
+        onClick={() => endConversation()}
         id={`bottomOfChat${str}`}
         style={{
           margin: '3vmin 0',
@@ -59,9 +90,10 @@ const styles = {
   buttonDiv: {
     width: '100%',
     display: 'flex',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
   },
   button: {
     margin: '25px 10px',
+    flexGrow: 1,
   },
 };
